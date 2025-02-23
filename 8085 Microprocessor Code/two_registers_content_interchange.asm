11c474db1ed41fd3382ef79f5802f4eb1b357b51
Swastik Bhattacharya <152956696+Swastik0710@users.noreply.github.com>
2025-02-23 12:10:26 +0530

    Update two_registers_content_interchange.asm

--- 8085 Microprocessor Code/two_registers_content_interchange.asm
+++ 8085 Microprocessor Code/two_registers_content_interchange.asm
@@ -5,9 +5,9 @@ MVI B, 44H      ; Load the value 44H in register B
 MVI C, 55H      ; Load the value 55H in register C
 
 ; Interchange the values of registers B and C
-MOV A, B        ; Moving the content of register B to the accumulator (A) {A = 44H}
-MOV B, C        ; Moving the content of register C to register B {B = 55H}
-MOV C, A        ; Moving the content of the accumulator (A) to register C {C = 44H}
-RST 1           ; Resetting the program
+MOV A, B        ; Move the content of register B to the accumulator (A) {A = 44H}
+MOV B, C        ; Move the content of register C to register B {B = 55H}
+MOV C, A        ; Move the content of the accumulator (A) to register C {C = 44H}
+RST 1           ; Reset the program
 
 ; End of code
