ed1ce235996dd9eb2433f008a182a6b08dc12802
Swastik Bhattacharya <152956696+Swastik0710@users.noreply.github.com>
2025-07-10 12:23:55 +0530

    Update exchange_upper_lower_nibble.asm

--- 8051 Microcontroller Code/exchange_upper_lower_nibble.asm
+++ 8051 Microcontroller Code/exchange_upper_lower_nibble.asm
@@ -1,9 +1,20 @@
 ; Write a program to exchange the upper and lower nibble of location 50H.
 
-MOV R0, #50H      ; Load the value 50H tO register R0 {R0 = 50H}
+; Method 1:
+
+MOV R0, #50H      ; Load the value 50H to register R0 {R0 = 50H}
 MOV A, @R0        ; Move the content of internal memory location pointed by R0 to the accumulator (A)
 SWAP A            ; Swap the upper and lower nibbles of the data stored in the accumulator (A)
 MOV @R0, A        ; Move the content of the accumulator (A) to the internal memory location pointed by R0
+LCALL 00BBH       ; Call a subroutine located at memory address 00BBH
+
+; End of code
+
+; Method 2:
+
+MOV A, 50H        ; Load the content of internal memory location 50H into the accumulator (A)
+SWAP A            ; Swap the upper and lower nibbles of the data stored in the accumulator (A)
+MOV 50H, A        ; Store the modified content of the accumulator (A) back to internal memory location 50H
 LCALL 00BBH       ; Call a subroutine located at memory address 00BBH
 
 ; End of code
