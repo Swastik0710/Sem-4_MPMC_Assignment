3718aa4e28a16c76874aaafef5ed3dda7d4d1e01
Swastik Bhattacharya <152956696+Swastik0710@users.noreply.github.com>
2025-07-10 12:52:43 +0530

    Update convert_packed_BCD_number.asm

--- 8051 Microcontroller Code/convert_packed_BCD_number.asm
+++ 8051 Microcontroller Code/convert_packed_BCD_number.asm
@@ -1,7 +1,7 @@
 ; Write a program to convert a packed BCD number available in RAM location 50H to its equivalent unpacked format and store from internal memory location 40H.
 
-MOV R0, #50H      ; Load the value 50H tO register R0 {R0 = 50H}
-MOV R1, #40H      ; Load the value 40H tO register R1 {R1 = 40H}
+MOV R0, #50H      ; Load the value 50H to register R0 {R0 = 50H}
+MOV R1, #40H      ; Load the value 40H to register R1 {R1 = 40H}
 MOV A, @R0        ; Move the content of internal memory location pointed by R0 to the accumulator (A)
 ANL A, #0FH       ; Performs bitwise AND operation between the accumulator (A) and 0FH {Mask the upper nibble to keep only the lower nibble (units digit) of the BCD}
 SWAP A            ; Swap the upper and lower nibbles of the data stored in the accumulator (A)
